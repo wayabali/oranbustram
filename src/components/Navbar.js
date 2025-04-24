e96052b6926059118400c7f4cca3db42b27466e7
unknown <ryadbali60@gmail.com>
2025-04-25 00:56:34 +0100

    last ryad2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <strong>TBM</strong> Transport
+          <strong>ORAN</strong> Smart Transport
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
